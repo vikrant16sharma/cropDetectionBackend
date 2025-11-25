cbd2ae8a98c059cf07000493ff273d669373f4f4
vikrant16sharma <s.vikrant3002@gmail.com>
2025-11-25 09:16:57 +0530

    remmove some labels

--- main.py
+++ main.py
@@ -18,25 +18,10 @@ LABELS = ["Apple___Apple_scab",
   "Apple___Cedar_apple_rust",
   "Apple___healthy",
 
-  "Blueberry___healthy",
-
-  "Cherry_(including_sour)___Powdery_mildew",
-  "Cherry_(including_sour)___healthy",
-
   "Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot",
   "Corn_(maize)___Common_rust_",
   "Corn_(maize)___Northern_Leaf_Blight",
   "Corn_(maize)___healthy",
-
-  "Grape___Black_rot",
-  "Grape___Esca_(Black_Measles)",
-  "Grape___Leaf_blight_(Isariopsis_Leaf_Spot)",
-  "Grape___healthy",
-
-  "Orange___Haunglongbing_(Citrus_greening)",
-
-  "Peach___Bacterial_spot",
-  "Peach___healthy",
 
   "Pepper,_bell___Bacterial_spot",
   "Pepper,_bell___healthy",
@@ -44,16 +29,7 @@ LABELS = ["Apple___Apple_scab",
   "Potato___Early_blight",
   "Potato___Late_blight",
   "Potato___healthy",
-
-  "Raspberry___healthy",
-
-  "Soybean___healthy",
-
-  "Squash___Powdery_mildew",
-
-  "Strawberry___Leaf_scorch",
-  "Strawberry___healthy",
-
+  
   "Tomato___Bacterial_spot",
   "Tomato___Early_blight",
   "Tomato___Late_blight",
